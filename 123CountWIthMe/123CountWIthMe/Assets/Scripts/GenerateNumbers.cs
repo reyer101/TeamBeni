51417496a0cf6cee05ec9aa5299de7306467d182
Reyerson <reyer101@mail.chapman.edu>
2017-04-18 18:03:36 -0700

    Initial Project Commit

---
+++ 123CountWIthMe/123CountWIthMe/Assets/Scripts/GenerateNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateNumbers : MonoBehaviour {
+
+    GameObject[] charObjects;
+    int[] numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8};
+    int[] shuffledNums;
+    
+
+	// Use this for initialization
+	void Start () {
+        shuffledNums = numbers.OrderBy(n => System.Guid.NewGuid()).ToArray();
+        charObjects = GameObject.FindGameObjectsWithTag("Char");
+        generateNumbers();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.G)) {            
+            generateNumbers();
+        }
+		
+	}
+
+    void generateNumbers() {
+        shuffledNums = numbers.OrderBy(n => System.Guid.NewGuid()).ToArray();
+        for (int i = 0; i < charObjects.Length; ++i) {
+            TextMesh num = charObjects[i].GetComponent<TextMesh>();
+            num.text = shuffledNums[i].ToString();   
+
+        }
+        
+
+        
+    }
+}
---
+++ 123CountWIthMe/123CountWIthMe/Assets/Scripts/SelectChar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectChar : MonoBehaviour {
+    TextMesh character;    
+
+	// Use this for initialization
+	void Start () {
+        character = gameObject.GetComponent<TextMesh>();        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown() {
+        Debug.Log(character.text + " selected");
+
+    }
+}
